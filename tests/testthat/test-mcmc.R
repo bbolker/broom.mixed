f084c9a3852323f3e18262e63d0ccbb6606b9b5c
Dieter Menne <dieter.menne@menne-biomed.de>
2018-01-09 15:04:00 +0100

    using check_tidy from helper-checkers in test-mcmc

--- tests/testthat/test-mcmc.R
+++ tests/testthat/test-mcmc.R
@@ -10,26 +10,24 @@ if (suppressPackageStartupMessages(require(rstan, quietly = TRUE)))  {
       # Make sure that (inst/)extdata/run_examples.R was run to generate rds
     rstan_example <- readRDS(system.file("extdata", "rstan_example.rds", package = "broom.mixed"))
     td <- broom::tidy(rstan_example) # For S3 only, avoid importing broom full namespace
-    expect_equal(names(td), c("term", "estimate", "std.error"))
-    expect_equal(nrow(td), 18)
+    # check_tidy from helper-checkers
+    check_tidy(td, 18, 3, c("term", "estimate", "std.error"))
     td_mcmc <- broom::tidyMCMC(rstan_example)
     expect_equal(td, td_mcmc)
     
     td <- tidyMCMC(rstan_example, index = TRUE)
-    expect_equal(names(td),
-                 c("term", "term0", "index", "estimate", "std.error"))
-    expect_equal(nrow(td), 18)
+    check_tidy(td, 18, 5, c("term", "term0", "index", "estimate", "std.error"))
+
     td <- tidyMCMC(rstan_example, drop.pars = NULL)
     expect_equal(td[19, "term"], "lp__")
+    
     td <- tidyMCMC(rstan_example, conf.int = TRUE)
-    expect_equal(names(td),
-                 c("term", "estimate", "std.error", "conf.low", "conf.high"))
-    expect_equal(nrow(td), 18)
+    check_tidy(td, 18, 5, c("term", "estimate", "std.error", "conf.low", "conf.high"))
+
     td <- tidyMCMC(rstan_example, rhat = TRUE)
-    expect_equal(names(td), c("term", "estimate", "std.error", "rhat"))
-    expect_equal(nrow(td), 18)
+    check_tidy(td, 18, 4, c("term", "estimate", "std.error", "rhat"))
+
     td <- tidyMCMC(rstan_example, ess = TRUE)
-    expect_equal(names(td), c("term", "estimate", "std.error", "ess"))
-    expect_equal(nrow(td), 18)
+    check_tidy(td, 18, 4, c("term", "estimate", "std.error", "ess"))
   })
 }

8970d7583396078df5db5c4abf9d980d8618a6eb
Alex Hayes <alexpghayes@gmail.com>
2018-08-07 15:06:09 -0700

    Re-export generics from broom

---
+++ R/broom.mixed-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom broom tidy
+#' @export
+broom::tidy
+
+#' @importFrom broom glance
+#' @export
+broom::glance
+
+#' @importFrom broom augment
+#' @export
+broom::augment

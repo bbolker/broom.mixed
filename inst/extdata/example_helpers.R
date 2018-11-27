3156f1b3ea8a5803d0af0cc5727407c209715d51
Ben Bolker <bbolker@gmail.com>
2018-11-27 13:29:42 -0500

    update examples

--- inst/extdata/example_helpers.R
+++ inst/extdata/example_helpers.R
@@ -1,5 +1,8 @@
+options(save.defaults=list(version=2, safe=TRUE))
+
 save_file <- function(..., pkg, type = "rda") {
   f <- file.path("inst", "extdata", sprintf("%s_example.%s", pkg, type))
+  cat("saving in serialization version 2")
   if (type == "rda") {
     save(..., file = f, version=2)
   } else {

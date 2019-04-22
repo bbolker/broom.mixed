43f1e60e1bd4bf45067d5cddabb466d050d6b47a
Ben Bolker <bbolker@gmail.com>
2019-04-22 12:36:16 -0400

    add sep2/document separators for ran_vals term names

--- R/hooks.R
+++ R/hooks.R
@@ -1,3 +1,7 @@
 .onLoad <- function(libname, pkgname) {
-  options(broom.mixed.sep1 = "__")
+    ## separator between type (sd/var/cor/cov) and following elements in
+    ## ran_pars terms
+    options(broom.mixed.sep1 = "__")
+    ## separator between elements in cor/cov terms
+    options(broom.mixed.sep2 = ".")
 }

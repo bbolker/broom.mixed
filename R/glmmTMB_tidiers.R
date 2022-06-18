cecd997bf2a9b5ce8350328dc290738c05f8223d
Ben Bolker <bbolker@gmail.com>
2022-06-17 21:45:20 -0400

    update to work with new (ci_tweaks) glmmTMB confint

--- R/glmmTMB_tidiers.R
+++ R/glmmTMB_tidiers.R
@@ -103,8 +103,15 @@ tidy.glmmTMB <- function(x, effects = c("ran_pars", "fixed"),
                          exponentiate = FALSE,
                          ...) {
 
-  ## FIXME:  cleanup
-  ##   - avoid (as.)data.frame
+    safe_confint <- function(...) {
+        args <- list(...)
+        if (packageVersion("glmmTMB") >= "1.1.4") {
+            args <- c(args, list(include_mapped = TRUE))
+        }
+        do.call(confint, args)
+    }
+    ## FIXME:  cleanup
+    ##   - avoid (as.)data.frame
 
   ## R CMD check false positives
   condsd <- condval <- grp <- grpvar <-
@@ -148,25 +155,25 @@ tidy.glmmTMB <- function(x, effects = c("ran_pars", "fixed"),
     # p-values may or may not be included
     # HACK: use the columns from the conditional component, preserving previous behaviour
     if (conf.int) {
-      for (comp in component) {
-        cifix <- confint(x,
-          method = tolower(conf.method),
-          level = conf.level,
-          component = comp,
-          estimate = FALSE,
-          ## conditional/zi components
-          ## include random-effect parameters
-          ## as well, don't want those right now ...
-          parm = seq(nrow(ret[[comp]])), ...
-        ) %>%
-          as.data.frame(stringsAsFactors = FALSE) %>%
-          setNames(c("conf.low", "conf.high"))
-        ret[[comp]] <- bind_cols(
-          ret[[comp]],
-          cifix
-        )
-      }
-    }
+        for (comp in component) {
+            cifix <- safe_confint(x,
+                            method = tolower(conf.method),
+                            level = conf.level,
+                            component = comp,
+                            estimate = FALSE,
+                            ## conditional/zi components
+                            ## include random-effect parameters
+                            ## as well, don't want those right now ...
+                            parm = seq(nrow(ret[[comp]])),
+                            ...) %>%
+                as.data.frame(stringsAsFactors = FALSE) %>%
+                setNames(c("conf.low", "conf.high"))
+            ret[[comp]] <- bind_cols(
+                ret[[comp]],
+                cifix
+            )
+        } ## component
+    } ## conf.int
     ret_list$fixed <- bind_rows(ret, .id = "component")
 
     if (exponentiate) {
@@ -247,7 +254,7 @@ tidy.glmmTMB <- function(x, effects = c("ran_pars", "fixed"),
         if (utils::packageVersion("glmmTMB")<="0.2.2.0") {
              thpar <- which(names(x$obj$par)=="theta")
         }
-        ciran_t <- confint(x,
+        ciran_t <- safe_confint(x,
                           ## for next glmmTMB (> 0.2.3) can be "theta_",
                           parm = thpar,
                           method = conf.method,
@@ -255,7 +262,7 @@ tidy.glmmTMB <- function(x, effects = c("ran_pars", "fixed"),
                           estimate = FALSE,
                           ...
                           )
-        ciran_s <- confint(x,
+        ciran_s <- safe_confint(x,
                           parm = "sigma",
                           method = conf.method,
                           level = conf.level,

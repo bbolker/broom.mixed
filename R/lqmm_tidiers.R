35221e49436d430e73870100a3e5e02af338f7f5
Ben Bolker <bbolker@gmail.com>
2022-07-13 10:50:43 -0400

    lqmm tidiers

---
+++ R/lqmm_tidiers.R
@@ -0,0 +1,16 @@
+## from David Luke Thiessen, https://stackoverflow.com/questions/72514230/is-it-possible-to-use-lqmm-with-a-mira-object
+tidy.lqmm <- function(x, conf.int = FALSE, conf.level = 0.95, ...) {
+  broom:::as_tidy_tibble(data.frame(
+    estimate = coef(x),
+    std.error = sqrt(
+      diag(summary(x, covariance = TRUE, 
+                   R = 50)$Cov[names(coef(x)),
+                               names(coef(x))]))))
+}
+glance.lqmm <- function(x, ...) {
+  broom:::as_glance_tibble(
+    logLik = as.numeric(stats::logLik(x)),
+    df.residual = summary(x)$rdf,
+    nobs = stats::nobs(x),
+    na_types = "rii")
+}
